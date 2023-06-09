d70c9886f05cb8c6d662f90bf9d6c45d7999e737
richard-tanakov <tanakovr@yandex.ru>
2023-06-09 21:31:28 +0300

    Создание и добавление зомби

--- Contract.sol
+++ Contract.sol
@@ -1,14 +1,22 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.19; // Объявление версии (Без неё не создаш контракта)
 
-contract ZombieFactory {
+
+contract ZombieFactory { // Создание контракта, зомби
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
-       
-    struct Zombie {
-        string  name;
+
+    struct Zombie { // Сам зомби
+        string name;
         uint dna;
-   }
+    }
+
     Zombie[] public zombies;
+
+    function createZombie(string _name, uint _dna) {
+        zombies.push(Zombie(_name, _dna)); // добавление новых.
+    }
+
+}
 
 }
