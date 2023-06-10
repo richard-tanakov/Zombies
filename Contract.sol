4f29afe57f772549f135937504c4ac44d5f42fc5
richard-tanakov <tanakovr@yandex.ru>
2023-06-10 19:09:50 +0300

    Контракт Зомби готов,переходим к Web3.js

--- Contract.sol
+++ Contract.sol
@@ -1,22 +1,35 @@
-pragma solidity ^0.4.19; // Объявление версии (Без неё не создаш контракта)
+pragma solidity >=0.4.19;
 
+contract ZombieFactore {
+    event NewZombie(uint zombieId, string name, uint dna); //Создание события
+    uint dnaDigins = 16;
+    // задали длину днк Зомби
 
-contract ZombieFactory { // Создание контракта, зомби
-
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
-
-    struct Zombie { // Сам зомби
-        string name;
-        uint dna;
-    }
-
-    Zombie[] public zombies;
-
-    function createZombie(string _name, uint _dna) {
-        zombies.push(Zombie(_name, _dna)); // добавление новых.
-    }
-
+    uint dnaModulus = 10 ** dnaDigins;
+    // Проверка что днк равна 16 числам.
 }
+   struct Zombie {
+      string name;
+      uint dna;
+      // создается структура зомби
+   }
+    Zombie[] public zombies; 
+    // Создание массива
+
+   function _createZomie (string _name, uint _dna) private{
+    // Функция для производства Зомби (Она закрытая)
+    uint id = zombies.push(Zombie(_name, _dna)) -1;
+    enit NewZombie(id,_name,_dna);
+   }
+   function _generateRandomDna(string _str) private view returns (uint){
+    // Функция для создания рандомных значений ДНК
+     uint rand =  uint(keccak256(_str)); //При помощи кеширования создаём рандомные 16 цифр
+        return rand % dnaModulus;
+   }
+     function createRandomZombie(string _name) public {
+        uuint id = zombies.push(Zombie(_name, _dna)) - 1;
+        NewZombie(id, _name, _dna); 
+        // Всю башку сломал! Нужно быть аккуратней с пробелами.если пробел стоит там где его не должно быть будет ошибка. 
+    }
 
 }
